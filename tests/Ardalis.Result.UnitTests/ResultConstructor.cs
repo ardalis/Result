e79504e7e785551bd472fa422822bf2ca6a5c7e6
Steve Smith <steve@kentsmiths.com>
2019-11-22 12:19:46 -0500

    Added constructor tests

--- tests/Ardalis.Result.UnitTests/ResultConstructor.cs
+++ tests/Ardalis.Result.UnitTests/ResultConstructor.cs
@@ -6,11 +6,44 @@ namespace Ardalis.Result.UnitTests
     public class ResultConstructor
     {
         [Fact]
-        public void InitializesStronglyTypedValue()
+        public void InitializesStronglyTypedStringValue()
         {
-            var result = new Result<string>("test string");
+            string expectedString = "test string";
+            var result = new Result<string>(expectedString);
 
-            Assert.Equal("test string", result.Value);
+            Assert.Equal(expectedString, result.Value);
         }
+
+        [Fact]
+        public void InitializesStronglyTypedIntValue()
+        {
+            int expectedInt = 123;
+            var result = new Result<int>(expectedInt);
+
+            Assert.Equal(expectedInt, result.Value);
+        }
+
+        [Fact]
+        public void InitializesStronglyTypedObjectValue()
+        {
+            var expectedObject = new TestObject();
+            var result = new Result<TestObject>(expectedObject);
+
+            Assert.Equal(expectedObject, result.Value);
+        }
+
+        private class TestObject
+        {
+        }
+
+        [Fact]
+        public void InitializesValueToNullGivenNullConstructorArgument()
+        {
+            var result = new Result<object>(null);
+
+            Assert.Null(result.Value);
+        }
+
+
     }
 }
