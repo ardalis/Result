0f73b84bf1cf8919b5f1ce86fb5ebf84c886d1da
Steve Smith <steve@kentsmiths.com>
2020-05-15 13:16:47 -0400

    Adding separate package with filters and such

---
+++ src/Ardalis.Result.AspNetCore/TranslateResultToActionResultAttribute.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Ardalis.Result.SampleWeb
+{
+    public class TranslateResultToActionResultAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (!((context.Result as ObjectResult).Value is IResult result)) return;
+
+            if (!(context.Controller is ControllerBase controller)) return;
+
+            if (result.Status == ResultStatus.NotFound)
+                context.Result = controller.NotFound();
+
+            if (result.Status == ResultStatus.Invalid)
+            {
+                foreach (var error in result.ValidationErrors)
+                {
+                    (context.Controller as ControllerBase).ModelState.AddModelError(error.Key, error.Value);
+                }
+
+                context.Result = controller.BadRequest(controller.ModelState);
+            }
+
+            if(result.Status == ResultStatus.Ok)
+            {
+                context.Result = new OkObjectResult(result.GetValue());
+            }
+        }
+    }
+}
