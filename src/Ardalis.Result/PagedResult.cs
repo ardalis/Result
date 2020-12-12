4a59c79a0ee69bd677621413618ad9eb1e861e74
ShadyNagy <info@shadynagy.com>
2020-12-12 11:43:33 +0200

    PagedResult added.

---
+++ src/Ardalis.Result/PagedResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Ardalis.Result
+{
+    public class PagedResult<T> : Result<T>
+    {
+        public PagedResult(PagedInfo pagedInfo, T value) : base(value)
+        {
+            PagedInfo = pagedInfo;
+        }
+
+        public PagedInfo PagedInfo { get; private set; }
+
+        public void SetPagedInfo(PagedInfo pagedInfo)
+        {
+            PagedInfo = pagedInfo;
+        }
+    }
+}
