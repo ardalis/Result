806f62929a3c3301cc51f7057ca0bd46a4a0db10
Stephan Filipczyk <stephan.filipczyk@werma.com>
2022-05-06 09:49:11 +0200

    Support FluentValidation ErrorCode

--- src/Ardalis.Result/ValidationError.cs
+++ src/Ardalis.Result/ValidationError.cs
@@ -4,6 +4,7 @@
     {
         public string Identifier { get; set; }
         public string ErrorMessage { get; set; }
+        public string ErrorCode { get; set; }
         public ValidationSeverity Severity { get; set; } = ValidationSeverity.Error;
     }
 }
