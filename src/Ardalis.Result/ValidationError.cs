43eb6944c9d7ad5abaa569134a3b593e97bdebbd
Dani Hengeveld <dani.hengeveld@infosupport.com>
2023-10-14 14:39:00 +0200

    add Invalid(params ValidationError[]) factory method.

--- src/Ardalis.Result/ValidationError.cs
+++ src/Ardalis.Result/ValidationError.cs
@@ -2,6 +2,23 @@
 {
     public class ValidationError
     {
+        public ValidationError()
+        {
+        }
+
+        public ValidationError(string errorMessage)
+        {
+            ErrorMessage = errorMessage;
+        }
+
+        public ValidationError(string identifier, string errorMessage, string errorCode, ValidationSeverity severity)
+        {
+            Identifier = identifier;
+            ErrorMessage = errorMessage;
+            ErrorCode = errorCode;
+            Severity = severity;
+        }
+
         public string Identifier { get; set; }
         public string ErrorMessage { get; set; }
         public string ErrorCode { get; set; }
