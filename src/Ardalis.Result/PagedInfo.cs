9bfa4f8f2ee30ad9f0f31e4fde812d7c6413dd2a
Mischa Weerwag <m.weerwag@litebit.nl>
2024-02-28 01:54:36 +0100

    Fix Serialization always results in a Success object. (#166)
    Co-authored-by: Steve Smith <steve@kentsmiths.com>

--- src/Ardalis.Result/PagedInfo.cs
+++ src/Ardalis.Result/PagedInfo.cs
@@ -1,4 +1,6 @@
-﻿namespace Ardalis.Result
+﻿using System.Text.Json.Serialization;
+
+namespace Ardalis.Result
 {
     public class PagedInfo
     {
@@ -11,9 +13,13 @@
             TotalRecords = totalRecords;
         }
 
+        [JsonInclude] 
         public long PageNumber { get; private set; }
+        [JsonInclude] 
         public long PageSize { get; private set; }
+        [JsonInclude] 
         public long TotalPages { get; private set; }
+        [JsonInclude] 
         public long TotalRecords { get; private set; }
 
         public PagedInfo SetPageNumber(long pageNumber)
