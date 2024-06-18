bb930b30fe6439113ef0fe21390d1f6548efe492
Chris Ingham <chris.ingham@live.com>
2024-06-18 14:21:41 -0600

    Fix unhandled ResultStatus.NoContent in MinimalApiResultExtensions.ToMinimalApiResult (#192)
    https://github.com/ardalis/Result/issues/191

---
+++ tests/Ardalis.Result.AspNetCore.UnitTests/MinimalApiResultExtensionsCoverage.cs
@@ -0,0 +1,38 @@
+#if NET7_0_OR_GREATER
+
+using Ardalis.Result.AspNetCore;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Ardalis.Result.AspNetCore.UnitTests;
+
+public class MinimalApiResultExtensionsCoverage : BaseResultConventionTest
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public MinimalApiResultExtensionsCoverage(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    private class TestResult(ResultStatus status) : Result(status);
+    
+    [Fact]
+    public void ToMinimalApiResultHandlesAllResultStatusValues()
+    {
+        foreach (ResultStatus resultStatus in Enum.GetValues(typeof(ResultStatus)))
+        {
+            Result result = new TestResult(resultStatus);
+            try
+            {
+                Microsoft.AspNetCore.Http.IResult minimalApiResult = result.ToMinimalApiResult();
+            }
+            catch (NotSupportedException e)
+            {
+                Assert.Fail(
+                    $"Unhandled ResultStatus {resultStatus} in MinimalApiResultExtensions.ToMinimalApiResult: {e}");
+            }
+        }
+    }
+}
+#endif
