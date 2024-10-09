f75be4f3e036d35124a5081523b69e35185e9649
Jose Martinez <jose.martinez@spreetail.com>
2024-10-09 10:49:30 -0500

    Add Map method to map Generic Result to Result

--- src/Ardalis.Result/ResultExtensions.cs
+++ src/Ardalis.Result/ResultExtensions.cs
@@ -43,5 +43,39 @@ namespace Ardalis.Result
                     throw new NotSupportedException($"Result {result.Status} conversion is not supported.");
             }
         }
+        
+        /// <summary>
+        /// Transforms a Result's type from a source type to a non-generic Result type.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        /// <exception cref="NotSupportedException"></exception> 
+        public static Result Map<TSource>(this Result<TSource> result)
+        {
+            switch (result.Status)
+            {
+                case ResultStatus.Ok: return Result.Success();
+                case ResultStatus.NotFound: return result.Errors.Any()
+                    ? Result.NotFound(result.Errors.ToArray())
+                    : Result.NotFound();
+                case ResultStatus.Unauthorized: return result.Errors.Any()
+                    ? Result.Unauthorized(result.Errors.ToArray())
+                    : Result.Unauthorized();
+                case ResultStatus.Forbidden: return result.Errors.Any()
+                    ? Result.Forbidden(result.Errors.ToArray())
+                    : Result.Forbidden();
+                case ResultStatus.Invalid: return Result.Invalid(result.ValidationErrors);
+                case ResultStatus.Error: return Result.Error(new ErrorList(result.Errors.ToArray(), result.CorrelationId));
+                case ResultStatus.Conflict: return result.Errors.Any()
+                    ? Result.Conflict(result.Errors.ToArray())
+                    : Result.Conflict();
+                case ResultStatus.CriticalError: return Result.CriticalError(result.Errors.ToArray());
+                case ResultStatus.Unavailable: return Result.Unavailable(result.Errors.ToArray());
+                case ResultStatus.NoContent: return Result.NoContent();
+                default:
+                    throw new NotSupportedException($"Result {result.Status} conversion is not supported.");
+            }
+        }
     }
 }
