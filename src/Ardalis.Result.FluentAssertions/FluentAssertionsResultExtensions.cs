d8724114a5243a1d1a4de84fe17bceca98e80a5e
Maxence <maxence.martin.info@gmail.com>
2025-01-04 22:02:46 +0100

    Add ResultErrorShouldBeError

--- src/Ardalis.Result.FluentAssertions/FluentAssertionsResultExtensions.cs
+++ src/Ardalis.Result.FluentAssertions/FluentAssertionsResultExtensions.cs
@@ -1,6 +1,12 @@
-﻿namespace Ardalis.Result.FluentAssertions;
+﻿using FluentAssertions;
+using FluentAssertions.Primitives;
+
+namespace Ardalis.Result.FluentAssertions;
 
 public static class FluentAssertionsResultExtensions
 {
-    
+    public static AndConstraint<BooleanAssertions> ShouldBeError(this Result result)
+    {
+        return result.IsError().Should().BeTrue();
+    }
 }
---
+++ tests/Ardalis.Result.FluentAssertions.UnitTests/FluentAssertionsResultExtensionsTests.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+using FluentAssertions.Primitives;
+using Xunit;
+
+namespace Ardalis.Result.FluentAssertions.UnitTests;
+
+public class FluentAssertionsResultExtensionsTests
+{
+    [Fact]
+    public void ResultErrorShouldBeError()
+    {
+        Result.Error().ShouldBeError().Should().BeOfType(typeof(AndConstraint<BooleanAssertions>));
+    }
+}
