fa50ebbeec9aeafda1aebd929ac52a78511f3c09
Maxence <maxence.martin.info@gmail.com>
2025-01-11 10:29:32 +0100

    Add success

--- tests/Ardalis.Result.FluentAssertions.UnitTests/FluentAssertionsResultExtensionsTests.cs
+++ tests/Ardalis.Result.FluentAssertions.UnitTests/FluentAssertionsResultExtensionsTests.cs
@@ -1,4 +1,6 @@
+using Moq;
 using Xunit;
+using static Ardalis.Result.Result;
 
 namespace Ardalis.Result.FluentAssertions.UnitTests;
 
@@ -11,25 +13,25 @@ public class FluentAssertionsResultExtensionsTests
     [Fact]
     public void ConflictResult_ShouldBeFailure()
     {
-        Result.Conflict().ShouldBeFailure();
+        Conflict().ShouldBeFailure();
     }
 
     [Fact]
     public void ConflictResultWithErrorMessages_ShouldBeFailureWithErrorMessages()
     {
-        Result.Conflict(ErrorMessage).ShouldBeFailure(ErrorMessage);
+        Conflict(ErrorMessage).ShouldBeFailure(ErrorMessage);
     }
 
     [Fact]
     public void ConflictResult_ShouldBeConflict()
     {
-        Result.Conflict().ShouldBeConflict();
+        Conflict().ShouldBeConflict();
     }
 
     [Fact]
     public void ConflictResultWithErrorMessages_ShouldBeConflictWithErrorMessages()
     {
-        Result.Conflict(ErrorMessage).ShouldBeConflict(ErrorMessage);
+        Conflict(ErrorMessage).ShouldBeConflict(ErrorMessage);
     }
 
     //CriticalError
@@ -37,25 +39,25 @@ public class FluentAssertionsResultExtensionsTests
     [Fact]
     public void CriticalErrorResult_ShouldBeFailure()
     {
-        Result.CriticalError().ShouldBeFailure();
+        CriticalError().ShouldBeFailure();
     }
 
     [Fact]
     public void CriticalErrorResultWithErrorMessages_ShouldBeFailureWithErrorMessages()
     {
-        Result.CriticalError(ErrorMessage).ShouldBeFailure(ErrorMessage);
+        CriticalError(ErrorMessage).ShouldBeFailure(ErrorMessage);
     }
 
     [Fact]
     public void CriticalErrorResult_ShouldBeCriticalError()
     {
-        Result.CriticalError().ShouldBeCriticalError();
+        CriticalError().ShouldBeCriticalError();
     }
 
     [Fact]
     public void CriticalErrorResultWithErrorMessages_ShouldBeCriticalErrorWithErrorMessages()
     {
-        Result.CriticalError(ErrorMessage).ShouldBeCriticalError(ErrorMessage);
+        CriticalError(ErrorMessage).ShouldBeCriticalError(ErrorMessage);
     }
 
 
@@ -64,25 +66,25 @@ public class FluentAssertionsResultExtensionsTests
     [Fact]
     public void ErrorResult_ShouldBeFailure()
     {
-        Result.Error().ShouldBeFailure();
+        Error().ShouldBeFailure();
     }
 
     [Fact]
     public void ErrorResultWithErrorMessages_ShouldBeFailureWithErrorMessages()
     {
-        Result.Error(ErrorMessage).ShouldBeFailure(ErrorMessage);
+        Error(ErrorMessage).ShouldBeFailure(ErrorMessage);
     }
 
     [Fact]
     public void ErrorResult_ShouldBeError()
     {
-        Result.Error().ShouldBeError();
+        Error().ShouldBeError();
     }
 
     [Fact]
     public void ErrorResultWithErrorMessage_ShouldBeErrorWithErrorMessage()
     {
-        Result.Error(ErrorMessage).ShouldBeError(ErrorMessage);
+        Error(ErrorMessage).ShouldBeError(ErrorMessage);
     }
 
     [Fact]
@@ -90,7 +92,7 @@ public class FluentAssertionsResultExtensionsTests
     {
         var errorList = new ErrorList([ErrorMessage], "CorrelationId");
         
-        Result.Error(errorList).ShouldBeError(errorList);
+        Error(errorList).ShouldBeError(errorList);
     }
 
     [Fact]
@@ -98,7 +100,7 @@ public class FluentAssertionsResultExtensionsTests
     {
         var errorList = new ErrorList([ErrorMessage], "CorrelationId");
 
-        Result.Error(errorList).ShouldBeError([ErrorMessage], "CorrelationId");
+        Error(errorList).ShouldBeError([ErrorMessage], "CorrelationId");
     }
 
     //Forbidden
@@ -106,25 +108,25 @@ public class FluentAssertionsResultExtensionsTests
     [Fact]
     public void ForbiddenResult_ShouldBeFailure()
     {
-        Result.Forbidden().ShouldBeFailure();
+        Forbidden().ShouldBeFailure();
     }
 
     [Fact]
     public void ForbiddenResultWithMessage_ShouldBeFailureWithMessage()
     {
-        Result.Forbidden(ErrorMessage).ShouldBeFailure(ErrorMessage);
+        Forbidden(ErrorMessage).ShouldBeFailure(ErrorMessage);
     }
 
     [Fact]
     public void ForbiddenResult_ShouldBeForbidden()
     {
-        Result.Forbidden().ShouldBeForbidden();
+        Forbidden().ShouldBeForbidden();
     }
 
     [Fact]
     public void ForbiddenResultWithMessage_ShouldBeForbiddenWithMessage()
     {
-        Result.Forbidden(ErrorMessage).ShouldBeForbidden(ErrorMessage);
+        Forbidden(ErrorMessage).ShouldBeForbidden(ErrorMessage);
     }
 
     //Invalid
@@ -133,106 +135,111 @@ public class FluentAssertionsResultExtensionsTests
     [Fact]
     public void InvalidResult_ShouldBeFailure()
     {
-        Result.Invalid().ShouldBeFailure();
+        Invalid().ShouldBeFailure();
     }
 
     [Fact]
     public void InvalidResultWithValidationError_ShouldBeFailure()
     {
-        Result.Invalid(new ValidationError("IDENTIFIER", "ERROR_MESSAGE")).ShouldBeFailure();
+        Invalid(new ValidationError("IDENTIFIER", "ERROR_MESSAGE")).ShouldBeFailure();
     }
 
     [Fact]
     public void InvalidResult_ShouldBeInvalid()
     {
-        Result.Invalid().ShouldBeInvalid();
+        Invalid().ShouldBeInvalid();
     }
 
     [Fact]
     public void InvalidResultWithValidationError_ShouldBeInvalidWithValidationError()
     {
         var validationError = new ValidationError("IDENTIFIER", "ERROR_MESSAGE");
-        Result.Invalid(validationError).ShouldBeInvalid(validationError);
+        Invalid(validationError).ShouldBeInvalid(validationError);
     }
     
     //NotFound
 
-
     [Fact]
     public void NotFoundResult_ShouldBeFailure()
     {
-        Result.NotFound().ShouldBeFailure();
+        NotFound().ShouldBeFailure();
     }
 
     [Fact]
     public void NotFoundResultWithMessage_ShouldBeFailureWithMessage()
     {
-        Result.NotFound(ErrorMessage).ShouldBeFailure(ErrorMessage);
+        NotFound(ErrorMessage).ShouldBeFailure(ErrorMessage);
     }
 
     [Fact]
     public void NotFoundResult_ShouldBeNotFound()
     {
-        Result.NotFound().ShouldBeNotFound();
+        NotFound().ShouldBeNotFound();
     }
 
     [Fact]
     public void NotFoundResultWithErrorMessages_ShouldBeNotFoundWithErrorMessages()
     {
-        Result.NotFound(ErrorMessage).ShouldBeNotFound(ErrorMessage);
+        NotFound(ErrorMessage).ShouldBeNotFound(ErrorMessage);
     }
 
     //Unauthorized
 
-
     [Fact]
     public void UnauthorizedResult_ShouldBeFailure()
     {
-        Result.Unauthorized().ShouldBeFailure();
+        Unauthorized().ShouldBeFailure();
     }
 
     [Fact]
     public void UnauthorizedResultWithErrorMessages_ShouldBeFailureWithErrorMessages()
     {
-        Result.Unauthorized(ErrorMessage).ShouldBeFailure(ErrorMessage);
+        Unauthorized(ErrorMessage).ShouldBeFailure(ErrorMessage);
     }
 
     [Fact]
     public void UnauthorizedResult_ShouldBeUnauthorized()
     {
-        Result.Unauthorized().ShouldBeUnauthorized();
+        Unauthorized().ShouldBeUnauthorized();
     }
 
     [Fact]
     public void UnauthorizedResultWithErrorMessages_ShouldBeUnauthorizedWithErrorMessages()
     {
-        Result.Unauthorized(ErrorMessage).ShouldBeUnauthorized(ErrorMessage);
+        Unauthorized(ErrorMessage).ShouldBeUnauthorized(ErrorMessage);
     }
-
 
     //Unavailable
 
     [Fact]
     public void UnavailableResult_ShouldBeFailure()
     {
-        Result.Unavailable().ShouldBeFailure();
+        Unavailable().ShouldBeFailure();
     }
 
     [Fact]
     public void UnavailableResultWithErrorMessages_ShouldBeFailureWithErrorMessages()
     {
-        Result.Unavailable(ErrorMessage).ShouldBeFailure(ErrorMessage);
+        Unavailable(ErrorMessage).ShouldBeFailure(ErrorMessage);
     }
 
     [Fact]
     public void UnavailableResult_ShouldBeUnavailable()
     {
-        Result.Unavailable().ShouldBeUnavailable();
+        Unavailable().ShouldBeUnavailable();
     }
 
     [Fact]
     public void UnavailableResultWithErrorMessages_ShouldBeUnavailableWithErrorMessages()
     {
-        Result.Unavailable(ErrorMessage).ShouldBeUnavailable(ErrorMessage);
+        Unavailable(ErrorMessage).ShouldBeUnavailable(ErrorMessage);
+    }
+
+    //Success
+
+    [Fact]
+    public void Foo()
+    {
+        Created(It.IsAny<object>()).ShouldBeSuccess(ErrorMessage);
     }
 }
